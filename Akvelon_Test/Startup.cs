e55ef1edafef41023160c89a48d0ac4097f23fb4
GachiSolid <miftahovsalavat@gmail.com>
2021-07-15 20:01:35 +0300

    add xml comments

--- Akvelon_Test/Startup.cs
+++ Akvelon_Test/Startup.cs
@@ -12,7 +12,9 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
@@ -41,6 +43,9 @@ namespace Akvelon_Test
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Akvelon_Test", Version = "v1" });
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
             });
             services.AddSwaggerGenNewtonsoftSupport();
         }
